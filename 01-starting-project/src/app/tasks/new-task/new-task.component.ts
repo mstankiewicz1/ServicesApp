7ac52abab118a2acee1e07149341a7fa31456a7b
Marek <marekstankiewicz6@wp.pl>
2025-01-13 17:02:19 +0100

    service progress

---
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TaskService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  private tasksService: TaskService;
+
+  constructor() {
+    this.tasksService = new TaskService();
+  }
+
+  onAddTask(title: string, description: string) {
+    this.tasksService.addTask({ title, description });
+    this.formEl()?.nativeElement.reset();
+  }
+}
